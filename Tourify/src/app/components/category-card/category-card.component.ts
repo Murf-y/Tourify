50325cc81a444fabf442d80c96ac2c62f3dcfece
Murf-y <charbelfayad64@gmail.com>
2022-12-08 10:00:39 +0200

    finished category card :fire:

--- Tourify/src/app/components/category-card/category-card.component.ts
+++ Tourify/src/app/components/category-card/category-card.component.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-category-card',
+  selector: 'category-card',
   templateUrl: './category-card.component.html',
   styleUrls: ['./category-card.component.scss'],
 })
 export class CategoryCardComponent implements OnInit {
-
-  constructor() { }
+  image_path: string = 'assets/images/category_card_default_img.png';
+  category_name: string = 'Nature';
+  constructor() {}
 
   ngOnInit() {}
-
 }
