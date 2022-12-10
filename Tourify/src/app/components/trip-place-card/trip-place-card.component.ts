e7dcd143e14603dec4cd7d7dfb3329e03831de48
Murf-y <charbelfayad64@gmail.com>
2022-12-10 17:30:59 +0200

    add components for trips :tada:

---
+++ Tourify/src/app/components/trip-place-card/trip-place-card.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-trip-place-card',
+  templateUrl: './trip-place-card.component.html',
+  styleUrls: ['./trip-place-card.component.scss'],
+})
+export class TripPlaceCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
