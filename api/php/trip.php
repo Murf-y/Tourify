e469d0b252c3a50a6c852a2e0d887b745e1931a6
Murf-y <charbelfayad64@gmail.com>
2022-12-10 16:38:03 +0200

    params

--- api/php/trip.php
+++ api/php/trip.php
@@ -10,13 +10,18 @@ header("Content-Type: application/json; charset=UTF-8");
 
 
 if (
-    $_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['name'])
+    $_SERVER['REQUEST_METHOD'] == 'POST'
+    && isset($_POST['user_id'])
+    && isset($_POST['name'])
     && isset($_POST['start_date'])
     && isset($_POST['end_date'])
 ) {
+    $user_id = $_POST['user_id'];
     $name = $_POST['name'];
     $start_date = $_POST['start_date'];
     $end_date = $_POST['end_date'];
+
+    $sql = "INSERT INTO trips (name, start_date, end_date) VALUES ('$name', '$start_date', '$end_date')";
 } else {
     echo json_encode(array(
         "status" => 400,
