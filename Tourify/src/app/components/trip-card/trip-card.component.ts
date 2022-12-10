69c3f0919e581502cefe95515f97089e394c796c
Murf-y <charbelfayad64@gmail.com>
2022-12-10 17:52:09 +0200

    trip card

--- Tourify/src/app/components/trip-card/trip-card.component.ts
+++ Tourify/src/app/components/trip-card/trip-card.component.ts
@@ -1,14 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Trip } from 'app/models/trip';
 
 @Component({
-  selector: 'app-trip-card',
+  selector: 'trip-card',
   templateUrl: './trip-card.component.html',
   styleUrls: ['./trip-card.component.scss'],
 })
 export class TripCardComponent implements OnInit {
-
-  constructor() { }
+  @Input() trip!: Trip;
+  constructor() {}
 
   ngOnInit() {}
-
 }
--- Tourify/src/app/pages/trips/trips.module.ts
+++ Tourify/src/app/pages/trips/trips.module.ts
@@ -7,14 +7,10 @@ import { IonicModule } from '@ionic/angular';
 import { TripsPageRoutingModule } from './trips-routing.module';
 
 import { TripsPage } from './trips.page';
+import { TripCardComponent } from 'app/components/trip-card/trip-card.component';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    TripsPageRoutingModule
-  ],
-  declarations: [TripsPage]
+  imports: [CommonModule, FormsModule, IonicModule, TripsPageRoutingModule],
+  declarations: [TripsPage, TripCardComponent],
 })
 export class TripsPageModule {}
