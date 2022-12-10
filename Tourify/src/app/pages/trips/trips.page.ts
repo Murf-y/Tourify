10851253903e7e6605e6179385be385cdaa3cbda
Murf-y <charbelfayad64@gmail.com>
2022-12-10 17:28:15 +0200

    link trip api :rocket:

--- Tourify/src/app/pages/trips/trips.page.ts
+++ Tourify/src/app/pages/trips/trips.page.ts
@@ -1,18 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Trip } from 'app/models/trip';
+import { User } from 'app/models/user';
+import { TripCrudService } from 'app/services/tripCrud.service';
 
 @Component({
   selector: 'app-trips',
   templateUrl: './trips.page.html',
   styleUrls: ['./trips.page.scss'],
 })
-export class TripsPage implements OnInit {
+export class TripsPage {
   emptyTrips: boolean = true;
   trips: Trip[] = [];
-  constructor(private router: Router) {}
 
-  ngOnInit() {}
+  user!: User;
+
+  constructor(private router: Router, private tripService: TripCrudService) {
+    this.user = JSON.parse(sessionStorage.getItem('current_user') || '{}');
+
+    if (!this.user || !this.user.id) {
+      this.router.navigate(['/login']);
+    }
+  }
+
+  ionViewWillEnter() {
+    this.tripService.getAll(this.user.id).subscribe((res) => {
+      console.log(res);
+      this.trips = res.data.trips;
+      this.emptyTrips = this.trips.length === 0;
+    });
+  }
 
   goToCreateTrip() {
     this.router.navigate(['/create-trip']);
