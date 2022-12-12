dd979c0570a09747992f76573bd3a8626d5cf062
Murf-y <charbelfayad64@gmail.com>
2022-12-12 15:50:40 +0200

    finish trip places page :fire:

--- Tourify/src/app/pages/trip/trip.page.ts
+++ Tourify/src/app/pages/trip/trip.page.ts
@@ -1,6 +1,7 @@
 import { LocationStrategy } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { ItemReorderEventDetail } from '@ionic/angular';
 import { Trip } from 'app/models/trip';
 import { User } from 'app/models/user';
 import { TripCrudService } from 'app/services/tripCrud.service';
@@ -41,6 +42,17 @@ export class TripPage implements OnInit {
     this.route.params.subscribe((params) => {
       this.trip_id = params['id'];
     });
+  }
+
+  handleReorder(ev: CustomEvent<ItemReorderEventDetail>) {
+    // The `from` and `to` properties contain the index of the item
+    // when the drag started and ended, respectively
+    console.log('Dragged from index', ev.detail.from, 'to', ev.detail.to);
+
+    // Finish the reorder and position the item in the DOM based on
+    // where the gesture ended. This method can also be called directly
+    // by the reorder group
+    ev.detail.complete();
   }
 
   ngOnInit(): void {
