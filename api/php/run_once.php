1b8ba7d21f12b156d1c9d10a569c25d3e33148f3
Murf-y <charbelfayad64@gmail.com>
2022-12-15 17:12:53 +0200

    Add places in run_once file :rocket:

--- api/php/run_once.php
+++ api/php/run_once.php
@@ -21,4 +21,27 @@
 //     $connection->query($sql);
 // }
 
+
+// THIS WILL INSERT THE PLACES
+
+
+// $connection->query("
+// INSERT INTO `places` (`id`, `name`, `overview`, `district`, `city`, `photo_url`, `added_at`, `latitude`, `longitude`, `category_id`) VALUES
+// (1, 'Jeita Grotto', 'Limestone grotto with 2 caves, one of the world\'s largest stalactites, gift shops & a restaurant.', 'Keserwan', 'Jeita', '/tourify/api/content/assets/places/jeitagrotto.jpg', '2022-12-09 17:25:00', 33.943771, 35.640827, 1),
+// (3, 'Zaitunay Bay', 'The Zaitunay Bay an exclusive and luxurious destination rivaling the best the world has to offer.', 'Beirut', 'Beirut', '/tourify/api/content/assets/places/zeitunabay.jpg', '2022-12-09 17:49:50', 33.902267, 35.498211, 3),
+// (4, 'Pigeon Rocks', 'Raouche is mostly famous for its Pigeon Rocks, a 60-meter high offshore rock couple, which was formed in the prehistoric era by a geologic movement.', 'Beirut', 'Beirut', '/tourify/api/content/assets/places/pigeonrocks.jpg', '2022-12-09 17:49:50', 33.889614, 35.470303, 1),
+// (7, 'Ocean Blue', 'Located in Jbeil, Ocean Blue Beach Resort Jbeil offers a private beach, an outdoor pool and a restaurant. WiFi access is available in this resort.', 'Keserwan', 'Byblos', '/tourify/api/content/assets/places/ocen_blue.JPG', '2022-12-15 14:21:54', 34.109558, 35.651203, 6),
+// (8, 'Se', 'Sebyoceanblue is a beachfront restaurant, lounge sea food and international cuisine.', 'Keserwan', 'Byblos', '/tourify/api/content/assets/places/se.jpg', '2022-12-15 14:28:32', 34.105347, 35.650608, 5),
+// (9, 'Fidar Beach House', 'One of the best seafood restaurants in Lebanon. It is located directly on the beach and overlooks the Fidar public beach bay. Customers have access to cushioned beach chairs and fresh water shower and can enjoy taking a dip in the beach before or after en', 'Jbeil', 'Halat', '/tourify/api/content/assets/places/fidar.jpg', '2022-12-15 14:31:02', 34.098404, 35.650612, 5),
+// (10, 'Murray', 'A well located restaurant that serves premium Lebanese and international food. Great food, large portions and relatively affordable. This places has an indoor as well as outdoor seating areas. For shisha lovers this place can be a good spot to relax and s', 'Keserwan', 'Jal El Dib', '/tourify/api/content/assets/places/muray.JPG', '2022-12-15 14:34:39', 33.922760, 35.588139, 5),
+// (11, 'Ramlet El Bayda', 'Nice sandy beach with clear waters', 'Beirut', 'Beirut', '/tourify/api/content/assets/places/ramle.jpg', '2022-12-15 14:38:22', 33.884022, 35.476791, 6),
+// (12, 'Caprice Beirut', 'Caprice is located on the seaside of Jal el Dib, making it easy to access whether from Jounieh or from Beirut; the lounge is a midway between the two.', 'Beirut', 'Jal El Dib', '/tourify/api/content/assets/places/capricejpg.jpg', '2022-12-15 14:40:45', 33.901711, 35.556801, 3),
+// (13, 'club7 night club', 'club7 night club ; Address, : Ghazir, Lebanon ; Coordinate, : 34.0067468, 35.6445787 ; Phone, : +961 78 864 685 ', 'Keserwan', 'Ghazir', '/tourify/api/content/assets/places/club7.jpg', '2022-12-15 14:43:05', 34.009163, 35.644745, 3),
+// (14, 'Jounieh Public Park', 'One of the best public gardens that represents an open space for rest and relaxation in the Lebanese city of Jounieh, where plants and flowers are dis.', 'Keserwan', 'Jounieh', '/tourify/api/content/assets/places/2021-02-09.jpg', '2022-12-15 14:44:57', 33.992123, 35.640064, 2),
+// (15, 'Ballouneh Park', 'One of the fewest parks in any town in Lebanon. Well maintained. There are a few tables and benches if you want to take in some food and eat there. ', 'Keserwan', 'Ballouneh', '/tourify/api/content/assets/places/baloune.jpg', '2022-12-15 14:47:18', 33.957825, 35.670635, 2),
+// (16, 'BURJ on BAY', 'Burj On Bay, a luxurious 5 Star Hotel located in Lebanon - Tabarja, next to Casino Du Liban with Sea view rooms, Junior suits, Delux and Executive rooms.', 'Keserwan', 'Tabarja', '/tourify/api/content/assets/places/bay.jpg', '2022-12-15 14:49:33', 34.040916, 35.632339, 4),
+// (17, 'Le Six Resort', 'In our Resort Hotel, located on the best spots of Batroun in Lebanon, including an amazing and wide outdoor pool, we represent you a unique service, which reflects our passion for entertainment and luxury.', 'North Lebanon', 'Batroun', '/tourify/api/content/assets/places/six.jpg', '2022-12-15 14:51:53', 34.241966, 35.637829, 4),
+// (18, 'Naas Camping', 'This is a very special place for camping surrounded by Pine trees. Trail. To navigate, press the arrow keys. Latest Members. John. Nadine J Salameh.', 'Mount Leb', 'Saqyet El Misk', '/tourify/api/content/assets/places/nass.jpeg', '2022-12-15 14:54:28', 33.916237, 35.683228, 7),
+// (19, 'Ctrek Camping', 'Family oriented and dog friendly camping sites at Neurum Creek Bush Retreat, on the sunshine coast, south-east Queensland.', 'North Leb', 'Kahf El Malloul', '/tourify/api/content/assets/places/ctrek.jpg', '2022-12-15 14:56:33', 34.348309, 35.962379, 7)");
+
 ?>
